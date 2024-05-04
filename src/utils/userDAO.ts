633ef334c24ec58bb9e036dff5d7422271429662
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-05-04 15:53:11 +0300

    chore: add swagger file

--- src/utils/userDAO.ts
+++ src/utils/userDAO.ts
@@ -3,4 +3,8 @@ import { UserAttributes } from "./types";
 export interface DAO {
   createUser(user: UserAttributes);
   findByCredentials(email: string, password: string);
+  logoutUser();
+  readUser();
+  updateUser();
+  deleteUser();
 }
