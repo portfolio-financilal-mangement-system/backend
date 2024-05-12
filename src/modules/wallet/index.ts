5224aa4fff2347d42e292ad53375e8662424cb2d
el-sayedmahmoud <elsayedmahmoud132001@gmail.com>
2024-05-12 16:14:59 +0300

    add stocks

--- src/modules/wallet/index.ts
+++ src/modules/wallet/index.ts
@@ -2,7 +2,13 @@
 import express from "express";
 import { json } from "body-parser";
 import { logRequest } from "./middleware";
-import { createPortfolio, getPortfolio } from "./wallet.controller";
+import {
+  createPortfolio,
+  getPortfolio,
+  getStocks,
+  getStockByName,
+  searchStocks,
+} from "./wallet.controller";
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +18,12 @@ app.use(logRequest);
 
 app.post("/portfolio", createPortfolio);
 app.get("/portfolio/:userId", getPortfolio);
+
+
+app.get("/stocks", getStocks);
+app.get("/stocks/:companyName", getStockByName);
+app.get("/stocks/search", searchStocks);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
